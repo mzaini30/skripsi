96936e5e713c4fa9716813a1bdebe6e399a058f1
mzaini30 <muhzaini30@gmail.com>
2019-06-19 10:26:17 +0800

    oke

--- web - penelitian awal/application/views/template/global.php
+++ web - penelitian awal/application/views/template/global.php
@@ -3,6 +3,7 @@
 <head>
 	<title><?= $judul ?>Esai</title>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 	<link rel="stylesheet" type="text/css" href="<?= base_url() ?>aset/vendor/bootstrap/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="<?= base_url() ?>aset/app/style.css">
 </head>
